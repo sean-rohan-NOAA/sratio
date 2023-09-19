251be9dfd708032081bc91db61ae78227841101e
sean-rohan-NOAA <sean.rohan@noaa.gov>
2023-09-19 11:29:11 -0700

    Add maps and comments

--- analysis/3_goddard_hauls.R
+++ analysis/3_goddard_hauls.R
@@ -21,27 +21,27 @@ hauls_1995 <- RODBC::sqlQuery(channel = channel,
                               query = "select * from racebase.haul
                                         where cruise = 199501
                                         and vessel in (88, 89)
-                                        and haul_type = 7
-                                        and haul < 267") |>
+                                        and haul_type = 7") |>
   dplyr::filter(!is.na(NET_WIDTH),
                 !is.na(DISTANCE_FISHED),
                 PERFORMANCE >= 0) |>
   dplyr::filter(DISTANCE_FISHED > 0,
                 START_TIME > as.POSIXct("1995-07-24 PDT")) |>
-  dplyr::filter(!(HAUL %in% c(218) & VESSEL == 88))
+  dplyr::filter(!(HAUL %in% c(218) & VESSEL == 88),
+                HAUL < 267)
 
-
+# Make an sf object showing haul paths (estimated as a straight line between start and end positions)
 hauls_1995_sf <- hauls_1995 |>
   dplyr::select(START_LONGITUDE, END_LONGITUDE, START_LATITUDE, END_LATITUDE, VESSEL, HAUL, STATIONID, START_TIME, DURATION) |>
   tidyr::pivot_longer(cols = c("START_LONGITUDE", "END_LONGITUDE")) |>
   dplyr::rename(LONGITUDE = value) |>
   dplyr::mutate(LATITUDE = ifelse(name == "START_LONGITUDE", START_LATITUDE, END_LATITUDE)) |>
-  dplyr::mutate(START_END = ifelse(name == "START_LONGITUDE", "Start", "End")) |>
-  dplyr::select(-START_LATITUDE, -END_LATITUDE, -name, -START_END) |>
+  dplyr::select(-START_LATITUDE, -END_LATITUDE, -name) |>
   sf::st_as_sf(coords = c("LONGITUDE", "LATITUDE"), crs = "EPSG:4326") |>
   sf::st_transform(crs = "EPSG:3338") |>
   dplyr::group_by(VESSEL, HAUL, STATIONID, START_TIME, DURATION) |>
   dplyr::summarise(n = n()) |>
+  dplyr::select(-n) |>
   sf::st_cast(to = "LINESTRING")
 
 
@@ -50,5 +50,14 @@ plotly::ggplotly(
 ggplot() +
   geom_sf(data = hauls_1995_sf,
           mapping = aes(color = factor(VESSEL),
-                        text = HAUL))
+                        text = paste0(HAUL, " ", as.character(START_TIME)))) +
+  scale_color_discrete(name = "Vessel")
 )
+
+ggplot() +
+  geom_sf(data = map_layers$survey.strata,
+          fill = NA) +
+  geom_sf(data = hauls_1995_sf,
+          mapping = aes(color = factor(VESSEL),
+                        text = paste0(HAUL, " ", as.character(START_TIME)))) +
+  scale_color_discrete(name = "Vessel")
