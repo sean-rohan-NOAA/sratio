895a55962c02eaebffbf4775bd524e6681a8ca29
sean-rohan-NOAA <sean.rohan@noaa.gov>
2024-05-06 08:13:53 -0700

    Combined scope tables

--- analysis/shelf_slope/95_scope_table.R
+++ analysis/shelf_slope/95_scope_table.R
@@ -1,15 +1,23 @@
 library(sratio)
 
-scope_table <- readRDS(here::here("analysis", "shelf_slope", "data", "ss_haul.rds")) |> 
+# 2023 Scope Values
+scope_table_2023 <- readRDS(here::here("analysis", "shelf_slope", "data", "ss_haul.rds")) |> 
   dplyr::select(dplyr::all_of(c("BOTTOM_DEPTH", "GEAR", "WIRE_LENGTH"))) |>
   dplyr::inner_join(data.frame(GEAR = c(44, 172), GEAR_NAME = c("83-112", "PNE")))
 
 
+# EBS shelf and slope standard scope tables
+scope_table_standard <- readxl::read_xlsx(
+  here::here("analysis", "shelf_slope", "docs", "shelf_slope_table.xlsx")
+  )
+
+
+# Plot 2023 scope table
 ragg::agg_png(filename = here::here("analysis", "shelf_slope", "plots", "2023_depth_vs_scope.png"),
               width = 6, height = 4, units = "in", res = 180)
 print(
   ggplot() +
-    geom_point(data = scope_table,
+    geom_point(data = scope_table_2023,
                mapping = aes(x = BOTTOM_DEPTH, 
                              y = WIRE_LENGTH, 
                              color = GEAR_NAME, 
@@ -21,3 +29,7 @@ print(
     theme(plot.title = element_text(hjust = 0.5))
 )
 dev.off()
+
+
+
+
