b35fbeeceb83d02d0161fad527c1d547da777d58
sean-rohan-NOAA <sean.rohan@noaa.gov>
2024-07-30 16:17:17 -0700

    Bonus zones for 15/30

--- analysis/15_30/92_additional_sample_zones.R
+++ analysis/15_30/92_additional_sample_zones.R
@@ -55,8 +55,7 @@ hauls_2024 <- RODBC::sqlQuery(channel = con,
                               and PERFORMANCE >=0") |>
   dplyr::inner_join(stn_centroids)
 
-sampled_stations <- dplyr::filter(map_layers$survey.grid,
-                                  STATIONID %in% hauls_2024$STATIONID)
+sampled_stations <- sf::st_as_sf(hauls_2024, crs = "EPSG:3338", coords = c("X", "Y"))
 
 spread_2024 <- read.csv(file = "C:\\Users\\sean.rohan\\Work\\afsc\\trawlmetrics\\output\\race_data_edit_hauls_table_EBS_2024.csv") |>
   dplyr::select(VESSEL, CRUISE, HAUL, EDIT_NET_SPREAD)
@@ -68,9 +67,21 @@ catch_2024 <- dplyr::inner_join(catch_2024, spread_2024) |>
 idw_results <- vector(mode = "list",
                       length = length(spp))
 
+# Load sample zones
+sample_zones <- readxl::read_xlsx(here::here("analysis", "15_30", "data", "expanded_sampling_zones.xlsx"))
+
+sample_zones <- dplyr::inner_join(map_layers$survey.grid, sample_zones) |>
+  dplyr::group_by(CLUSTER, TARGET) |>
+  summarise(do_union = TRUE)
+
+sample_zone_centroid <- sf::st_centroid(sample_zones)
+
+pdf(file = here::here("analysis", "15_30", "plots", "map_idw_stations_2024.pdf"), 
+    width = 8,
+    height = 6,
+    onefile = TRUE)
+
 for(ii in 1:length(spp)) {
-  
-  ii <- 3
   
   sel_catch <- dplyr::filter(catch_2024, SPECIES_CODE == spp[ii]) |>
   dplyr::full_join(hauls_2024) |>
@@ -87,19 +98,51 @@ for(ii in 1:length(spp)) {
                          in.crs = "EPSG:3338",
                          key.title.units = "CPUE (kg/km2)",
                          extrapolation.grid.type = "sf")
+  
+  species_map <- idw_results[[ii]]$plot + 
+    geom_sf(data = sampled_stations, fill = NA, color = "black", size = 0.4) +
+      scale_x_continuous(limits = c(idw_results[[ii]]$map_layers$plot.boundary$x)) +
+      scale_y_continuous(limits = c(idw_results[[ii]]$map_layers$plot.boundary$y))
+
+  print(species_map)
 }
 
-idw_results[[ii]]$plot + 
-                   geom_sf(data = sampled_stations, fill = NA, color = "white") +
-                   geom_sf_text(data = map_layers$survey.grid |>
-                                  sf::st_centroid(),
-                                mapping = aes(label = STATIONID)) +
-                   scale_x_continuous(limits = c(idw_results[[ii]]$map_layers$plot.boundary$x)) +
-                   scale_y_continuous(limits = c(idw_results[[ii]]$map_layers$plot.boundary$y))
+dev.off()
 
 
+# Load sample zones 
 
+pdf(file = here::here("analysis", "15_30", "plots", "map_additional_zones_2024.pdf"), 
+    width = 8,
+    height = 6,
+    onefile = TRUE)
 
+for(ii in 1:length(spp)) {
+  
+  zone_map <- idw_results[[ii]]$plot +
+    geom_sf(data = sampled_stations, fill = NA, color = "black", size = rel(1.2)) +
+    geom_sf(data = sample_zones,
+            mapping = aes(color = factor(CLUSTER)),
+            fill = NA,
+            alpha = 0.5,
+            linewidth = 1.2) +
+    geom_sf_text(data = sample_zone_centroid,
+                 mapping = aes(label = CLUSTER,
+                               color = factor(CLUSTER)),
+                 face = "bold",
+                 size = 5) +
+    scale_color_tableau(name = "Cluster", guide = "none") +
+    scale_x_continuous(limits = c(idw_results[[ii]]$map_layers$plot.boundary$x)) +
+    scale_y_continuous(limits = c(idw_results[[ii]]$map_layers$plot.boundary$y))
+  
+  print(zone_map)
+  
+}
 
+dev.off()
 
+dir.create(here::here("analysis", "15_30", "output", "nav_files"), showWarnings = FALSE)
+
+sf::st_write(obj = sample_zones, 
+             dsn = here::here("analysis", "15_30", "output", "nav_files", "1530_bonus_zones.shp"))
 
