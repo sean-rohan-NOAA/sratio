7994d9687157bb1df4c99101ebedd03ffe3693bc
sean-rohan-NOAA <sean.rohan@noaa.gov>
2024-10-03 14:08:12 -0700

    Update with 2024 data

--- analysis/shelf_slope/17_ss_bias_table.R
+++ analysis/shelf_slope/17_ss_bias_table.R
@@ -3,11 +3,9 @@
 library(sratio)
 
 # Load built-in data sets
-catch_df <- sratio::data_1530$catch |>
-  dplyr::filter(CRUISE %in% use_cruises)
+catch_df <- sratio::data_ss$catch
 
-haul_df <- sratio::data_1530$haul |>
-  dplyr::filter(CRUISE %in% use_cruises)
+haul_df <- sratio::data_ss$haul
 
 sp_codes <- sort(unique(catch_df$SPECIES_CODE))
 
@@ -16,16 +14,16 @@ bias_table <- catch_df |>
   dplyr::select(MATCHUP, SPECIES_CODE, WEIGHT, MATCHUP, TREATMENT) |>
   dplyr::mutate(TREATMENT = paste0("WEIGHT_", TREATMENT)) |>
   tidyr::pivot_wider(names_from = TREATMENT, values_from = WEIGHT, values_fill = 0) |>
-  dplyr::inner_join(readRDS(file = here::here("analysis", "15_30", "output", "n_by_treatment_1530.rds")) |>
-                      dplyr::select(SPECIES_CODE, MATCHUP, AREA_SWEPT_KM2_15, AREA_SWEPT_KM2_30) |>
+  dplyr::inner_join(readRDS(file = here::here("analysis", "shelf_slope", "output", "n_by_treatment_ss.rds")) |>
+                      dplyr::select(SPECIES_CODE, MATCHUP, AREA_SWEPT_KM2_172, AREA_SWEPT_KM2_44) |>
                       unique() |>
                       dplyr::mutate(MATCHUP = as.numeric(as.character(MATCHUP)))) |>
-  dplyr::mutate(CPUE_30 = WEIGHT_30/AREA_SWEPT_KM2_30,
-                CPUE_15 = WEIGHT_15/AREA_SWEPT_KM2_15) |>
-  dplyr::mutate(ratio = CPUE_15/CPUE_30,
-                log_error = log10(CPUE_15+0.001)-log10(CPUE_30+0.001),
-                abs_error = abs(log10(CPUE_15)-log10(CPUE_30)),
-                sq_error = (CPUE_15-CPUE_30)^2) |>
+  dplyr::mutate(CPUE_172 = WEIGHT_172/AREA_SWEPT_KM2_172,
+                CPUE_44 = WEIGHT_44/AREA_SWEPT_KM2_44) |>
+  dplyr::mutate(ratio = CPUE_172/CPUE_44,
+                log_error = log10(CPUE_172+0.001)-log10(CPUE_44+0.001),
+                abs_error = abs(log10(CPUE_172)-log10(CPUE_44)),
+                sq_error = (CPUE_172-CPUE_44)^2) |>
   dplyr::group_by(SPECIES_CODE) |>
   dplyr::summarise(MEAN_RATIO = mean(ratio),
                    BIAS = 10^(mean(log_error)),
@@ -34,11 +32,11 @@ bias_table <- catch_df |>
   dplyr::mutate(COMMON_NAME = sratio:::species_code_label(SPECIES_CODE, type = "common_name")) |>
   dplyr::select(COMMON_NAME, BIAS, MEAN_RATIO, MAE, RMSE)
 
-lines <- c("CPUE comparison between 15 and 30 minute tows\n", "BIAS > 1 = 15 minutes higher\n", "MEAN_RATIO > 1 = 15 minutes higher\n\n\n")
-cat(lines, file = here::here("analysis", "15_30", "plots", "bias_table.csv"))
+lines <- c("CPUE comparison between PNE and 83-112 tows\n", "BIAS > 1 = PNE higher\n", "MEAN_RATIO > 1 = PNE higher\n\n\n")
+cat(lines, file = here::here("analysis", "shelf_slope", "plots", "bias_table.csv"))
 
 write.table(bias_table, 
-            file = here::here("analysis", "15_30", "plots", "bias_table.csv"), 
+            file = here::here("analysis", "shelf_slope", "plots", "bias_table.csv"), 
             append = TRUE, 
             row.names = FALSE, 
             sep = ",")
